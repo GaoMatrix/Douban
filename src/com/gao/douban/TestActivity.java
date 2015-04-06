99a30c5f1777f0368476c0a281987e80495141d3
GaoMatrix <gcquan08@gmail.com>
2015-04-06 15:18:28 +0800

    TabHost使用

---
+++ src/com/gao/douban/TestActivity.java
@@ -0,0 +1,19 @@
+
+package com.gao.douban;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("test1");
+        setContentView(tv);
+    }
+
+}
